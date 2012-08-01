beae96bf731e5c6c1557d5de9f961c5b02ecf2ff
Lars Strojny <lars@strojny.net>
2012-08-01 17:29:06 +0200

    Fixing vendor.sh dir handling

--- vendor.sh
+++ vendor.sh
@@ -3,9 +3,9 @@
 repo_install() {
     if [ -d "$3" ]; then
         echo "Updating $3"
-        cd $3
+        pushd $3
         [ "$1" = svn ] && svn update -q || git pull -q
-        cd ..
+        popd
     else
         echo "Installing $3"
         mkdir -p $3
@@ -13,11 +13,12 @@ repo_install() {
     fi
 }
 
-cd vendor
+pushd vendor
 repo_install git git://github.com/zendframework/zf2.git zf2
 repo_install svn http://framework.zend.com/svn/framework/standard/trunk/library/ zf1
 repo_install git git://github.com/kriswallsmith/Buzz.git buzz
 repo_install git git://github.com/guzzle/guzzle.git guzzle
 repo_install git git://github.com/symfony/EventDispatcher.git Symfony/Component/EventDispatcher
 repo_install git git://github.com/symfony/Validator.git Symfony/Component/Validator
-cd ..
+repo_install git git://github.com/Seldaek/monolog.git monolog
+popd
