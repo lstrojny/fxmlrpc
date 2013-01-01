98045d5eaeee1790b066154b1c0795fab3e74548
Lars Strojny <lars@strojny.net>
2013-01-02 00:55:06 +0100

    Happy New Year

--- src/fXmlRpc/Transport/CurlTransport.php
+++ src/fXmlRpc/Transport/CurlTransport.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2012
+ * Copyright (C) 2012 - 2013
  * cryptocompress <cryptocompress@googlemail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
