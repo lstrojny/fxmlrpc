f81bd8f7c386f305843a2af57585574e29235c48
Lars Strojny <lars@strojny.net>
2016-05-19 13:31:09 +0200

    Fix tests for 5.5 with lowest version config

--- tests/fXmlRpc/Integration/AbstractIntegrationTest.php
+++ tests/fXmlRpc/Integration/AbstractIntegrationTest.php
@@ -181,7 +181,7 @@ abstract class AbstractIntegrationTest extends AbstractClientBasedIntegrationTes
         return [
             new \fXmlRpc\Transport\HttpAdapterTransport(
                 $messageFactory,
-                new \Http\Adapter\Guzzle6\Client()
+                new \Http\Adapter\Guzzle6\Client(new \GuzzleHttp\Client())
             )
         ];
     }
